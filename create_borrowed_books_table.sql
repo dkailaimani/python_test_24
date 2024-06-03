08aa7b9a1673e00cc15c8debba722ce94cdf571f
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-06-03 14:53:59 -0500

    Create create_borrowed_books_table.sql

---
+++ create_borrowed_books_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE borrowed_books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    borrow_date DATE NOT NULL,
+    return_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
